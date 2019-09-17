5deac09907d73ce7ee054bb3afc7df63070766ed
Chris Jenkins <chrisj@scorpion.co>
2019-09-17 16:56:18 -0700

    initial
    test

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  title: 'Dev Blog', // Required
-  author: 'Ryan Fitzgerald', // Required
-  description: 'Full-stack Web Developer',
-  primaryColor: '#3498db', // Required
-  showHeaderImage: true,
+  title: 'nice.', // Required
+  author: 'Chris Jenkins', // Required
+  description: 'this is a personal website, NOT a reflection of my professional portfolio.',
+  primaryColor: '#27fdf5', // Required
+  showHeaderImage: false,
   showShareButtons: true,
   postsPerPage: 5, // Required
   social: {
-    website: 'https://ryanfitzgerald.ca',
-    github: 'https://github.com/ryanfitzgerald',
-    twitter: 'https://twitter.com/ryanafitzgerald',
-    linkedin: 'https://ca.linkedin.com/in/ryanafitzgerald',
+    website: 'https://jenkins24.com',
+    github: 'https://github.com/christopherjenkins',
+    twitter: 'https://twitter.com/echt_0',
+    linkedin: 'https://www.linkedin.com/in/chris-jenkins-01238559/',
   },
   pathPrefix: '/devblog',
-  siteUrl: 'https://ryanfitzgerald.github.io/devblog/',
+  siteUrl: 'https://jenkins24.com',
 };
